d7423d798d54cf9080d6e08fa81b73b80ad217fa
Hassan Belaqdour <hassanbelaqdour@gmail.com>
2025-04-07 16:21:35 +0100

    ajout de la migration pour la table courses pour gestion des cours

---
+++ eduquest/database/migrations/2025_04_07_152022_create_courses_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('level', ['beginner', 'intermediate', 'advanced']);
+            $table->foreignId('teacher_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+}
