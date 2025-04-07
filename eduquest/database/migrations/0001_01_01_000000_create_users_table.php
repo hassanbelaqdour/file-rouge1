abe07749574fcb150cb2f87d5c4955ca0cb0e42f
Hassan Belaqdour <hassanbelaqdour@gmail.com>
2025-04-07 16:14:34 +0100

    ajout de la migration users avec les champs role et statut du compte

--- eduquest/database/migrations/0001_01_01_000000_create_users_table.php
+++ eduquest/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['student', 'teacher', 'admin'])->default('student');
+            $table->enum('account_status', ['pending', 'approved'])->default('pending');
             $table->rememberToken();
             $table->timestamps();
         });
