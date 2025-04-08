f912ab3ca253186368451129fee7ef76f3bac300
Hassan Belaqdour <hassanbelaqdour@gmail.com>
2025-04-08 09:34:35 +0100

    ajout groupe guest et route afficher inscription

--- eduquest/routes/web.php
+++ eduquest/routes/web.php
@@ -6,3 +6,8 @@ use App\Http\Controllers\AuthController; // Ajoutez l'import si ce n'est pas dé
 Route::get('/', function () {
     return view('home');
 })->name('home');
+// ... (import et route '/')
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+});
