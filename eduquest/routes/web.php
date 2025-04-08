77b394650e0e4587af4d2f5b4d5da127b2cae2d6
Hassan Belaqdour <hassanbelaqdour@gmail.com>
2025-04-08 09:40:43 +0100

    ajout groupe d'auth et route de deconnexion

--- eduquest/routes/web.php
+++ eduquest/routes/web.php
@@ -24,3 +24,8 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [AuthController::class, 'login']); // <- Ajoutée
 });
+// ... (import, route '/', groupe guest)
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
