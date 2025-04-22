c6d8b118f2b74cf08ea21d3a216b7d6942801f3d
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-04-22 16:30:47 +0100

    importer le controller teachercontroller

--- eduquest/routes/web.php
+++ eduquest/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\TeacherController;
 
 Route::get('/', function () {
     return view('home');
