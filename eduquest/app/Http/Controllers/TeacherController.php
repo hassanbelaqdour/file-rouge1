76028c1d2791b499459d0f8705e507f72566cdfc
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-04-23 12:07:49 +0100

    ajout methode teacher courses pour recuperer et afficher les cours du professeur

--- eduquest/app/Http/Controllers/TeacherController.php
+++ eduquest/app/Http/Controllers/TeacherController.php
@@ -8,5 +8,11 @@ use Illuminate\Http\Request;
 
 class TeacherController extends Controller
 {
+    public function courses()
+    {
+        $courses = Course::where('teacher_id', auth()->id())->get();
+        return view('teacher.Courses', compact('courses'));
+    }
+
 
 }
