a5bf419802fa52a839f466c738950d91defb0652
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-04-25 16:46:54 +0100

    importation du model category dans controller de teachercontroller

--- eduquest/app/Http/Controllers/TeacherController.php
+++ eduquest/app/Http/Controllers/TeacherController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
