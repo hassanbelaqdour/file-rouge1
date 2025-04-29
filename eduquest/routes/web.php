271364f3c09e3abe5de6c1d5e6114c855d0cfad0
hassanbelaqdour <hassanbelaqdour@gmail.com>
2025-04-29 14:16:04 +0100

    affichage des details d'un cours pour le teacher

--- eduquest/routes/web.php
+++ eduquest/routes/web.php
@@ -77,7 +77,7 @@ Route::put('/categories/{category}', [CategoryController::class, 'update'])
 
 Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])
     ->name('categories.destroy');
-
+    Route::get('/teacher/courses/{course}', [TeacherController::class, 'showCourse'])->name('teacher.courses.show');
     Route::get('/teacher/courses/{course}/edit', [TeacherController::class, 'edit'])->name('teacher.courses.edit');
     Route::put('/teacher/courses/{course}', [TeacherController::class, 'update'])->name('teacher.courses.update');
     Route::delete('/teacher/courses/{course}', [TeacherController::class, 'destroy'])->name('teacher.courses.destroy');
